125e375cd207a8db805036d5e880d52800f8fd70
kutman2007 <116934243+kutman2007@users.noreply.github.com>
2022-12-13 17:14:52 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,144 @@
+let body = document.body
+let write = document.createElement('h1')
+write.classList='er'
+write.innerHTML='MY LIFE'
+body.append(write)
+let main = document.createElement('div')
+main.classList='main'
+body.append(main)
+let cub1 = document.createElement('div')
+cub1.classList='cub'
+main.append(cub1)
+let pop1 = document.createElement('nav')
+pop1.classList='pop'
+pop1.innerHTML='<span>7 : 15</span>'
+cub1.append(pop1)
+let  sss = document.createElement('div')
+sss.classList='png'
+sss.innerHTML='<img class="zet" src="./wake-up.png" alt="">'
+cub1.append(sss)
+let getup = document.createElement('nav')
+getup.classList='getup'
+getup.innerHTML='<h2>Getup</h2>'
+cub1.append(getup)
+let cub2 = document.createElement('div')
+cub2.classList='cub'
+main.append(cub2)
+let dad = document.createElement('nav')
+dad.classList='pop'
+dad.innerHTML='<span>8 : 30</span>'
+cub2.append(dad)
+let aaa = document.createElement('div')
+aaa.classList='png'
+aaa.innerHTML='<img class="zet" src="./businessman-going-to-work.png" alt="">'
+cub2.append(aaa)
+let go = document.createElement('nav')
+go.classList='go'
+go.innerHTML='<h2>Lets Go</h2>'
+dad.append(go)
+let cub3 = document.createElement('div')
+cub3.classList='cub'
+main.append(cub3)
+let mom = document.createElement('nav')
+mom.classList='pop'
+mom.innerHTML='<span class="what">WhatsApp</span>'
+cub3.append(mom)
+let = bbb = document.createElement('div')
+bbb.classList='png'
+bbb.innerHTML='<img class="zet" src="./whatsapp.png" alt="">'
+cub3.append(bbb)
+let clock = document.createElement('nav')
+clock.classList='call'
+clock.innerHTML='<h2>Call</h2>'
+cub3.append(clock)
+let cub4 = document.createElement('div')
+cub4.classList='cub'
+main.append(cub4)
+let sis = document.createElement('nav')
+sis.classList='pop'
+sis.innerHTML='<span class="tele">Telegram</span>'
+cub4.append(sis)
+let = fff = document.createElement('div')
+fff.classList='png'
+fff.innerHTML='<img class="zet" src="./telegram.png" alt="">'
+cub4.append(fff)
+let tele = document.createElement('nav')
+tele.classList='chat'
+tele.innerHTML='<h2>Chat</h2>'
+cub4.append(tele)
+let cub5 = document.createElement('div')
+cub5.classList='cub'
+main.append(cub5)
+let bro = document.createElement('nav')
+bro.classList='pop'
+bro.innerHTML='<span class="game">Game</span>'
+cub5.append(bro)
+let = ggg = document.createElement('div')
+ggg.classList='png'
+ggg.innerHTML='<img class="zet" src="./console.png" alt="">'
+cub5.append(ggg)
+let click = document.createElement('nav')
+click.classList='chat'
+click.innerHTML='<h2>Click</h2>'
+cub5.append(click)
+let cub6 = document.createElement('div')
+cub6.classList='cub'
+main.append(cub6)
+let man = document.createElement('nav')
+man.classList='pop'
+man.innerHTML='<span class="fut">Football</span>'
+cub6.append(man)
+let = hhh = document.createElement('div')
+hhh.classList='png'
+hhh.innerHTML='<img class="zet" src="./soccer-ball-variant.png" alt="">'
+cub6.append(hhh)
+let play = document.createElement('nav')
+play.classList='play'
+play.innerHTML='<h2>Play</h2>'
+cub6.append(play)
+let cub7 = document.createElement('div')
+cub7.classList='cub'
+main.append(cub7)
+let women = document.createElement('nav')
+women.classList='pop'
+women.innerHTML='<span class="fast">FastFood</span>'
+cub7.append(women)
+let = jjj = document.createElement('div')
+jjj.classList='png'
+jjj.innerHTML='<img class="zet" src="./fast-food.png" alt="">'
+cub7.append(jjj)
+let food = document.createElement('nav')
+food.classList='eat'
+food.innerHTML='<h2>Eat</h2>'
+cub7.append(food)
+let cub8 = document.createElement('div')
+cub8.classList='cub'
+main.append(cub8)
+let nan = document.createElement('nav')
+nan.classList='pop'
+nan.innerHTML='<span class="book">Book</span>'
+cub8.append(nan)
+let = kkk = document.createElement('div')
+kkk.classList='png'
+kkk.innerHTML='<img class="zet" src="./book.png" alt="">'
+cub8.append(kkk)
+let book = document.createElement('nav')
+book.classList='chat'
+book.innerHTML='<h2>Read</h2>'
+cub8.append(book)
+let cub9 = document.createElement('div')
+cub9.classList='cub'
+main.append(cub9)
+let gaf = document.createElement('nav')
+gaf.classList='pop'
+gaf.innerHTML='<span class="home">Home</span>'
+cub9.append(gaf)
+let = qqq = document.createElement('div')
+qqq.classList='png'
+qqq.innerHTML='<img class="zet" src="./homework.png" alt="">'
+cub9.append(qqq)
+let home = document.createElement('nav')
+home.classList='sleep'
+home.innerHTML='<h2>Homework</h2>'
+cub9.append(home)
+
